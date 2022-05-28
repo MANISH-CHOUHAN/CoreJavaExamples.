c3879245b8e75f417c93e1f33e5d6289fcc2d376
MANISH CHOUHAN <manish.19bcon323@jecrcu.edu.in>
2022-05-28 17:22:05 +0530

    Commited 28-05-2022

---
+++ src/com/ksj/sixthclass/LinkedListImplementation.java
@@ -0,0 +1,67 @@
+package com.ksj.sixthclass;
+
+public class LinkedListImplementation {
+    Node head;
+
+    public void insert(int x) {
+        Node new1 = new Node(x);
+        if (head == null) {
+            head = new1;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+
+            }
+            temp.next = new1;
+        }
+
+
+    }
+    public void print()
+    {
+        Node temp= head;
+        while (temp.next!=null)
+        {
+            System.out.print(temp.value+"->");
+            temp=temp.next;
+        }
+        System.out.println(temp.value);
+    }
+
+    public void insertAfter(int insertAftervalue, int valueToInsert)
+    {
+      Node newNode=new Node(valueToInsert);
+      Node tempNode=head;
+      while (tempNode.next!=null)
+      {
+
+          if(tempNode.value==insertAftervalue)
+
+          {
+            newNode.next=tempNode.next;
+            tempNode.next=newNode;
+
+          }
+          tempNode=tempNode.next;
+
+      }
+    }
+
+    public void delete(int x)
+    {
+
+    }
+
+    public static void main(String[] args) {
+        LinkedListImplementation ls=new LinkedListImplementation();
+        ls.insert(2);
+        ls.insert(3);
+        ls.insert(4);
+        ls.print();
+        ls.insertAfter(2,6);
+        ls.print();
+
+    }
+
+}
---
+++ src/com/ksj/sixthclass/Node.java
@@ -0,0 +1,13 @@
+package com.ksj.sixthclass;
+
+public class Node {
+    int value;
+    Node next;
+
+    Node(int value)
+    {
+        this.value=value;
+        next=null;
+
+    }
+}
